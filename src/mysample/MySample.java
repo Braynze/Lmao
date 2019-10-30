e134917320b6645bb28eadd8b898bb9f9dca3178
taruc <taruc@B14>
2019-10-30 11:01:03 +0800

    Alpha Testing

--- src/mysample/MySample.java
+++ src/mysample/MySample.java
@@ -17,6 +17,7 @@ public class MySample {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hi");
+        System.out.println("Hello from the other side.");
     }
     
 }
